3a6de94d623e1f7bf3398cfd97aab2360cd79735
luis_benvenuto <luiss.benvenuto@gmail.com>
2024-09-23 22:53:09 -0600

    Update ZonaC.cpp

--- ZonaC.cpp
+++ ZonaC.cpp
@@ -1,6 +1,36 @@
+void adelante(){
+    //inicializar motores
+    /* 
+    int t = 0; //- > segundos
+    while (t < 30){ //(estimado, puede variar, medir práctica)
+        if (lineaAbajo() == true){
+            //poner motores en nulo
+            t= 0;
+            while( t< 10){ //(tiempo estimado en regresar al centro)
+                //activar motoroes en sentido negativo (retroceder) <-
+                t++; 
+            }
+            t= 30;
+        }else{
+            //activar motores en sentido positivo (avanzar) ->
+            t++; 
+        }
+        
+    }
+    */
+    
+}
+void girarIzq(){
+}
+void girarDer(){
+}
 
 
 
+void recorrido(){
+
+
+}
 
 int main(){
 
