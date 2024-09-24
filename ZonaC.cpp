1e215217dca34de99134adbd7fe82fff2b103b1d
luiss_benvenuto <luiss.benvenuto@gmail.com>
2024-09-23 23:50:51 -0600

    avance

--- ZonaC.cpp
+++ ZonaC.cpp
@@ -1,5 +1,68 @@
+#include <iostream>
+
+int places[3][5] = {{0,0,0,0,0}
+                ,   {0,0,0,0,0}
+                ,   {0,0,0,0,0}};
+int posY = 1;
+int posX = 0;
 
 
+void adelante(){
+    //inicializar motores
+    /* 
+    int t = 0; //- > segundos
+    while (t < 30){ //(estimado, puede variar, medir práctica)
+        if (lineaAbajo() == true){
+            //poner motores en nulo
+            t= 0;
+            while( t< 10){ //(tiempo estimado en regresar al centro)
+                //activar motoroes en sentido negativo (retroceder) <-
+                t++; 
+            }
+            t= 30;
+        }else{
+            //activar motores en sentido positivo (avanzar) ->
+            t++; 
+        }
+        
+    }
+    */
+    
+}
+void girarIzq(){
+
+}
+void girarDer(){
+
+}
+bool paredAdelante(){
+    //ver si hay una pared para entrar al centro
+    return true;
+}
+bool cuadroNegro(){
+
+}
+
+//método para buscar y mapear todos 
+void search(){
+    if(places[posY][posX] == 0){
+        places[posY][posX] == 1;
+        
+        for(int i = 0; i< 4; i++){
+            if(paredAdelante() == false){
+                adelante();
+                search();
+                girarIzq();
+                girarIzq();
+                adelante();
+                girarIzq();
+                girarIzq();
+            }
+            girarIzq();
+        }
+    }
+
+ }
 
 
 int main(){
