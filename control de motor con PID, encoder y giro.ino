f3fa559bc6c1990573592e248617e5b052b7cbb0
Fernando94654 <A00841856@tec.mx>
2024-10-02 20:50:28 -0600

    Update control de motor con PID, encoder y giro.ino

--- control de motor con PID, encoder y giro.ino
+++ control de motor con PID, encoder y giro.ino
@@ -1,18 +1,4 @@
 #include <PID_v1.h>//libreria PID
-//MPU6050
-#include "Simple_MPU6050.h"
-#define MPU6050_ADDRESS_AD0_LOW     0x68 
-#define MPU6050_ADDRESS_AD0_HIGH    0x69 
-#define MPU6050_DEFAULT_ADDRESS     MPU6050_ADDRESS_AD0_LOW
-#define Three_Axis_Quaternions 3
-#define Six_Axis_Quaternions 6  
-Simple_MPU6050 mpu(Six_Axis_Quaternions);
-#define spamtimer(t) for (static uint32_t SpamTimer; (uint32_t)(millis() - SpamTimer) >= (t); SpamTimer = millis()) 
-#define printfloatx(Name,Variable,Spaces,Precision,EndTxt) print(Name); {char S[(Spaces + Precision + 3)];Serial.print(F(" ")); Serial.print(dtostrf((float)Variable,Spaces,Precision ,S));}Serial.print(EndTxt);
-int z_rotation;//angulo de rotacion
-float ypr[3] = { 0, 0, 0 };
-float xyz[3] = { 0, 0, 0 };
-//Motores
 const int ENA_MT1 = 50;
 const int ENA_MT2 = 51;
 const int IN1_MT = 52;
@@ -44,12 +30,9 @@ double Kd = 0.001; // Ganancia derivativa
 // Inicializa el PID
 PID myPID(&input, &output, &setpoint, Kp, Ki, Kd, DIRECT);
 
-int dt=500;
-int orientacion=0;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  inicializarMPU6050();//MPU6050 Inicializacion
   //motores
   pinMode(IN1_MT,OUTPUT);
   pinMode(IN2_MT,OUTPUT);
@@ -72,16 +55,11 @@ void setup() {
 }
 
 void loop() {
-  //codigo prueba
-  loop_mpu();
+  codigo prueba
   ahead(1);
   stop(300);
-  right();
-  stop(200);
   back(1);
-  stop(300);
-  left();
-  stop(200); 
+  stop(300);  
 }
 //CALIBRACION DE VELOCIDAD CON PID
 void calibracion_MT(){
@@ -141,7 +119,6 @@ void ahead(int x){//avance adelnate, la variable x indica cuantas unidades desea
   digitalWrite(IN3_MT,1);
   digitalWrite(IN4_MT,0);
   units(x);
-  stop(0);
 }
 
 void back(int x){//retroceso
@@ -150,7 +127,6 @@ void back(int x){//retroceso
   digitalWrite(IN3_MT,0);
   digitalWrite(IN4_MT,1);
   units(x);
-  stop(0);
 }
 void stop(int x){
   digitalWrite(IN1_MT,0);
@@ -160,212 +136,17 @@ void stop(int x){
   delay(x);
 
 }
-void setright(){//setear orientacion de motores
+void right(){
   digitalWrite(IN1_MT,0);
   digitalWrite(IN2_MT,0);
   digitalWrite(IN3_MT,1);
   digitalWrite(IN4_MT,0);
+
 }
-void setleft(){//setear orientacion de motores
+void left(){
   digitalWrite(IN1_MT,1);
   digitalWrite(IN2_MT,0);
   digitalWrite(IN3_MT,0);
   digitalWrite(IN4_MT,0);
-}
-void right(){//girar a la derecha
-  if(orientacion==0){
-    while(z_rotation<90){
-      loop_mpu();  
-      setright();
-    }
-    stop(0);
-    orientacion=90;
-  }
-  else if(orientacion==90){
-    while(z_rotation<=179.9){
-      loop_mpu();
-      setright();
-    }
-    stop(0);
-    orientacion=180;    
-  }
-  else if(orientacion==180){
-    while(z_rotation>-90.1 && z_rotation<-89.9){
-      loop_mpu();
-      setright();
-    }
-    stop(0);
-    orientacion=-90;    
-  }
-  else if(orientacion==-90){
-    while(z_rotation<0){
-      loop_mpu();
-      setright();
-    }
-    stop(0);
-    orientacion=180;    
-  }  
 
-}
-void left(){//girar a la izquierda
-    if(orientacion==0){
-    while(z_rotation>-90){
-      loop_mpu();
-      setleft();
-    }
-    stop(0);
-    orientacion=-90;
-  }
-  else if(orientacion==-90){
-    while(z_rotation>=-179.9){
-      loop_mpu();
-      setleft();
-    }
-    stop(0);
-    orientacion=180;    
-  }
-  else if(orientacion==180){
-    while(z_rotation>90){
-      loop_mpu();
-      setleft();
-    }
-    stop(0);
-    orientacion=90;    
-  }
-  else if(orientacion==90){
-    while(z_rotation>0){
-      loop_mpu();
-      setright();
-    }
-    stop(0);
-    orientacion=90;    
-  }  
-
-}
-//////////////7funciones de MPU6050/////////////////////
-void loop_mpu(){
-  digitalWrite(8, !digitalRead(8));
-  digitalWrite(9, !digitalRead(9));
-  mpu.dmp_read_fifo(0);
-  z_rotation=xyz[0];
-}
-
-void PrintValues(int32_t *quat, uint16_t SpamDelay = 100) {
-  uint8_t WhoAmI;
-  Quaternion q;
-  VectorFloat gravity;
-  
-  spamtimer(SpamDelay) {
-    mpu.WHO_AM_I_READ_WHOAMI(&WhoAmI);
-    mpu.GetQuaternion(&q, quat);
-    mpu.GetGravity(&gravity, &q);
-    mpu.GetYawPitchRoll(ypr, &q, &gravity);
-    mpu.ConvertToDegrees(ypr, xyz);
-
-  }
-}
-
-void ChartValues(int32_t *quat, uint16_t SpamDelay = 100) {
-  Quaternion q;
-  VectorFloat gravity;
-  float ypr[3] = { 0, 0, 0 };
-  float xyz[3] = { 0, 0, 0 };
-  spamtimer(SpamDelay) {
-    mpu.GetQuaternion(&q, quat);
-    mpu.GetGravity(&gravity, &q);
-    mpu.GetYawPitchRoll(ypr, &q, &gravity);
-    mpu.ConvertToDegrees(ypr, xyz);
-  }
-}
-
-void PrintAllValues(int16_t *gyro, int16_t *accel, int32_t *quat, uint16_t SpamDelay = 100) {
-  Quaternion q;
-  VectorFloat gravity;
-  float ypr[3] = { 0, 0, 0 };
-  float xyz[3] = { 0, 0, 0 };
-  spamtimer(SpamDelay) {
-    mpu.GetQuaternion(&q, quat);
-    mpu.GetGravity(&gravity, &q);
-    mpu.GetYawPitchRoll(ypr, &q, &gravity);
-    mpu.ConvertToDegrees(ypr, xyz);
-  }
-}
-
-void ChartAllValues(int16_t *gyro, int16_t *accel, int32_t *quat, uint16_t SpamDelay = 100) {
-  Quaternion q;
-  VectorFloat gravity;
-  float ypr[3] = { 0, 0, 0 };
-  float xyz[3] = { 0, 0, 0 };
-  spamtimer(SpamDelay) {
-    mpu.GetQuaternion(&q, quat);
-    mpu.GetGravity(&gravity, &q);
-    mpu.GetYawPitchRoll(ypr, &q, &gravity);
-    mpu.ConvertToDegrees(ypr, xyz);
-  }
-}
-
-void PrintQuaternion(int32_t *quat, uint16_t SpamDelay = 100) {
-  Quaternion q;
-  spamtimer(SpamDelay) {
-    mpu.GetQuaternion(&q, quat);
-  }
-}
-
-void PrintEuler(int32_t *quat, uint16_t SpamDelay = 100) {
-  Quaternion q;
-  float euler[3];         
-  float eulerDEG[3];         
-  spamtimer(SpamDelay) {
-    mpu.GetQuaternion(&q, quat);
-    mpu.GetEuler(euler, &q);
-    mpu.ConvertToDegrees(euler, eulerDEG);
-  }
-}
-
-void PrintRealAccel(int16_t *accel, int32_t *quat, uint16_t SpamDelay = 100) {
-  Quaternion q;
-  VectorFloat gravity;
-  VectorInt16 aa, aaReal;
-  spamtimer(SpamDelay) {
-    mpu.GetQuaternion(&q, quat);
-    mpu.GetGravity(&gravity, &q);
-    mpu.SetAccel(&aa, accel);
-    mpu.GetLinearAccel(&aaReal, &aa, &gravity);
-  }
-}
-
-
-void PrintWorldAccel(int16_t *accel, int32_t *quat, uint16_t SpamDelay = 100) {
-  Quaternion q;
-  VectorFloat gravity;
-  VectorInt16 aa, aaReal, aaWorld;
-  spamtimer(SpamDelay) {
-    mpu.GetQuaternion(&q, quat);
-    mpu.GetGravity(&gravity, &q);
-    mpu.SetAccel(&aa, accel);
-    mpu.GetLinearAccel(&aaReal, &aa, &gravity);
-    mpu.GetLinearAccelInWorld(&aaWorld, &aaReal, &q);
-  }
-}
-
-void print_Values (int16_t *gyro, int16_t *accel, int32_t *quat) {
-  uint8_t Spam_Delay = 10; // Built in Blink without delay timer preventing Serial.print SPAM
-  PrintValues(quat, Spam_Delay);
-}
-void inicializarMPU6050(){
-  pinMode(7,OUTPUT);
-  Serial.begin(115200);
-  Serial.println(F("Start:"));
-  mpu.begin();
-  mpu.SetAddress(MPU6050_DEFAULT_ADDRESS);
-  mpu.Set_DMP_Output_Rate_Hz(10);           // Set the DMP output rate from 200Hz to 5 Minutes.
-  pinMode(8, OUTPUT);
-  pinMode(9, OUTPUT);
-  digitalWrite(8, HIGH);
-  digitalWrite(9, LOW);
-  mpu.CalibrateMPU().load_DMP_Image();// Does it all for you with Calibration
-  digitalWrite(8, LOW);
-  digitalWrite(9, HIGH);
-  mpu.CalibrateMPU().Enable_Reload_of_DMP(Three_Axis_Quaternions).load_DMP_Image();// Does it all for you with Calibration
-  mpu.on_FIFO(print_Values);
 }
