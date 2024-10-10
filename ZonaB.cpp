fe481077e694a1b4725853c1e2e576499a2945d9
luis_benvenuto <luiss.benvenuto@gmail.com>
2024-10-10 01:11:24 -0600

    Ajuste Zona C

--- ZonaB.cpp
+++ ZonaB.cpp
@@ -2,6 +2,21 @@ void avanzar(){
     //avanzar una unidad
     //de primer punto rojo, al último
 }
+// algoritmo facil 
+
+//Presentar junta:
+/*
+Approach 1: si ambos son negros continuar y si ambos son blancos continuar, linea recta.  y tarde o temprano regresará al otro lado con el nodo correspondiente
+Approach 2: Si detecta dos negros, irse por la izquierda, y si no se mantiene la línea dar vuelta y buscar po el otro lado.
+*/
+
+
+
+void zonaB(){
+
+}
+
+
 bool existeLinea(){
     return true; 
 }
--- zonaCPrueba.cpp
+++ zonaCPrueba.cpp
@@ -36,7 +36,7 @@ int colorDet(int colors[3]){
 }
 
 // arriba, izquierda, abajo, derecha
-void search(bool visited[5][3], int x, int y, int directions[4][2], int colors[3], int backstep[5][3], int& count, bool& pathFound){
+void search(bool visited[5][3], int x, int y, int directions[4][2], int colors[3], int backstep[5][3], int count, bool& pathFound){
     colorDet(colors);
     visited[x][y] = true;
     if(!pathFound){
@@ -68,21 +68,14 @@ void search(bool visited[5][3], int x, int y, int directions[4][2], int colors[3
         count--;
     }
 }
-void fuga(int count, int x, int y, int Mcolor, bool foundColor, int directions[4][2], int backstep[5][3]){
+void fuga(int count, int x, int y, int directions[4][2], int backstep[5][3]){
     for(int i = 0; i < count-1; i++){
-        if(!foundColor){
-            if(getcolor() == Mcolor){
-                //encenderLed
-                //print Mcolor
-                Mcolor = 30;
-            }
-        }
         for (int j = 0; j < 4; j++){
             int newX = x + directions[0][0];
             int newY = y + directions[0][1];
             if (backstep[newX][newY] == i+1){
                 ahead(1);
-                j=4;
+                j=0;
             }else{
                 girar(directions);
             }
@@ -100,89 +93,9 @@ void zonaB() {
     // punto inicial
     int start_x = 0, start_y = 0;
     int colors[3] = {0};
+
     bool pathFound = false;
     search(visited, start_x, start_y, directions, colors, backstep, count, pathFound);
-    bool foundColor = false;
-    int Mcolor= 0; 
-    for(int i=0; i< 3-1; i++){
-        if(colors[i] < colors[i+1]){
-            Mcolor = i+1;
-        }
-    }
 
-    fuga(count, start_x, start_y, Mcolor, foundColor, directions, backstep);
+    fuga(count, start_x, start_y, directions, backstep);
 }
-/*#include <iostream>
-using namespace std; 
-//solo adelante y girar izq
-void adelante(){}
-void girarIzq(int directions[4][2]){
-    int tempx = directions[0][0];
-    int tempy = directions[0][1];
-    
-    for (int i = 0; i< 3; i++){
-        directions[i][0] = directions[i+1][0];
-        directions[i][1] = directions[i+1][1];
-    }
-    directions[3][0] = tempx;
-    directions[3][1] = tempy;
-}
-void retroceso(){}
-
-bool paredAdelante(){
-    //ver si hay una pared para entrar al centro
-    return true;
-}
-bool cuadroNegro(){
-    return true; 
-}
-int colorDet(){
-
-    return 0;
-}
-
-// arriba, izquierda, abajo, derecha
-
-void search(bool visited[5][3], int x, int y, int directions[4][2]){
-
-    colorDet();
-    visited[x][y] = true;
-
-    for (int i = 0; i<4; i++){
-        int newX = x + directions[0][0];
-        int newY = y + directions[0][1];
-        if((newX >= 0 || newY >=0 || newX<=5 || newY <= 3) && (paredAdelante() == false)){
-            bool negro = cuadroNegro();
-            if(negro == false){
-                if(visited[newX][newY] == false){
-                    adelante();
-                    search(visited, newX, newY, directions);
-                    retroceso();
-                }
-            }else{
-                visited[newX][newY] = true; 
-            } 
-        }             
-        girarIzq(directions);
-    }
-}
-
-int main() {
-    //adelante, izquierda, atras, derecha
-    int directions[4][2] = {{0, 1}, {-1, 0}, {0, -1}, {1, 0}};
-
-    int count = 0;
-    // arreglo para llevar el control de las celdas visitadas
-    bool visited[5][3] = {{false}}; 
-
-    // punto inicial
-    int start_x = 0, start_y = 0;
-
-    
-    // iniciar DFS
-    search(visited, start_x, start_y, directions);
-    
-    // imprimir matriz con los valores de distancia
-    return 0;
-}
-*/
