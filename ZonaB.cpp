025289fd9187c15d83d8978400a47287e4c6aaf5
luiss_benvenuto <luiss.benvenuto@gmail.com>
2024-11-01 18:49:49 -0600

    Update ZonaB.cpp
    avance zona B medio final

--- ZonaB.cpp
+++ ZonaB.cpp
@@ -36,7 +36,9 @@ void virarI(){
 
 void path(){
     // 0 no linea - 1 linea
+    // derecho
     int infra1=digitalRead(infrared1);
+    // izquierdo
     int infra2=digitalRead(infrared2);
     //0 blanco - 1 negro
     int color = blackWhite();
@@ -46,39 +48,40 @@ void path(){
     Serial.print(",");
     Serial.println(infra1);
      //básico
-    if (infra1 == 0 && color == 1 && infra2 == 0){
+    if (infra2 == 0 && color == 1 && infra1 == 0){
         ava();
     }//avanza V
-    else if(infra1 == 1 && color == 0 && infra2 == 1){
-        /*
+    else if(infra2 == 1 && color == 0 && infra1 == 1){
         bool found = false;
         virarI();
         while (!found){ //sacar un rango de pasos para llegar al final
             virarI();
             infra1=digitalRead(infrared1);
             color = blackWhite();
-            if(infra1 == 0 && color == 4){
-                break;
+            if(infra1 == 0 && color == 1){
+                found = true; 
             }
         }
-        */
-    }// Línea en la derecha
-    else if(infra1 == 0 && color == 1 && infra2 == 1){
+        
+    }// Línea en la izquierda
+    else if(infra2 == 1 && color == 1 && infra1 == 0){
         while(infra2 == 1){
             virarI();
             infra2=digitalRead(infrared2);
         }
-    }// Línea en la izquierda PID
-    else if(infra1 == 1 && color == 1 && infra2 == 0){
+    }// Línea en la derecha
+    else if(infra2 == 0 && color == 1 && infra1 == 1){
         while(infra1 == 1){
             virarD();
             infra1=digitalRead(infrared1);
         }
     } // Línea Punteada, Blanco los tres
-    else if(infra1 == 0 && color == 0 && infra2 == 0){
+    else if(infra2 == 0 && color == 0 && infra1 == 0){
+        /*
         int count = 0;
         bool linea = false; 
-        while (count < 100 && linea == false){
+        int time = millis();
+        while ( (millis - time) < 1000 && linea == false){
             ava();
             color = blackWhite();
             if(color == 0){
@@ -86,8 +89,10 @@ void path(){
             }
             count++; 
         }
+        */
+        ava();
     }// peor de los casos
-    else if (infra1 == 1 && color == 0 && infra2 == 1){
+    else if (infra2 == 1 && color == 1 && infra1 == 1){
         // tipos de patrones
         // tipo 1: linea perpendicular con línea consecuente en el frente.
         ava();
@@ -103,11 +108,11 @@ void path(){
         // tipo 2: circulo o cuadrado con el centro despejado sin línea, recorrer el contorno
         
     }// Error, 001 and 100 
-    else if (infra1 == 0 && color = 0 infra2 == 1){
-        virarD();
-    }
-    else if (infra1 == 1 && color == 0 infra2 == 0){
+    else if (infra2 == 0 && color = 0 infra1 == 1){
         virarI();
+    }
+    else if (infra2 == 1 && color == 0 infra1 == 0){
+        virarD();
     }
     // else{
     //     cout << "error";
@@ -119,43 +124,10 @@ void zonaB(){
     set_speed();
     int infra1=digitalRead(infrared1);
     int infra2=digitalRead(infrared2);
-    Serial.print(infra2);
+    Serial.print(infra1);
     Serial.print(",");
-    Serial.println(infra1);
-    if(infra1==0 && infra2==0){
-        digitalWrite(IN1_MT,0);
-        digitalWrite(IN2_MT,0);
-        digitalWrite(IN3_MT,0);
-        digitalWrite(IN4_MT,0);
-        digitalWrite(IN5_MT,0);
-        digitalWrite(IN6_MT,1);
-        digitalWrite(IN7_MT,0);
-        digitalWrite(IN8_MT,1);
+    Serial.println(infra2);
+    while(paredAdelante() == false){
+        path();
     }
-    else if(infra1==0 && infra2==1){
-    //   setleft();
-        digitalWrite(IN1_MT,0);
-        digitalWrite(IN2_MT,0);//GIRAR SOBRE EL EJE
-        digitalWrite(IN3_MT,0);
-        digitalWrite(IN4_MT,0);
-        digitalWrite(IN5_MT,0);
-        digitalWrite(IN6_MT,1);
-        digitalWrite(IN7_MT,1);
-        digitalWrite(IN8_MT,0);
-    }
-  else if(infra1==1 && infra2==0){
-    // setright();
-    digitalWrite(IN1_MT,0);
-    digitalWrite(IN2_MT,0);//GIRAR SOBRE EL EJE
-    digitalWrite(IN3_MT,0);
-    digitalWrite(IN4_MT,0);
-    digitalWrite(IN5_MT,1);
-    digitalWrite(IN6_MT,0);
-    digitalWrite(IN7_MT,0);
-    digitalWrite(IN8_MT,1);
-    
-  }
-  else if(infra1==1 && infra2==1){
-    stop(0);
-  }
 }
